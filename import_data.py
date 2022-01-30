ba9c4ec510850dd77885d7cfabf3f7ec63fde525
5hulse <simonhulse@protonmail.com>
2022-01-30 14:45:50 +0000

    Refactor code for importing image data.

--- import_data.py
+++ import_data.py
@@ -1,27 +1,35 @@
 # import_data.py
 # Simon Hulse
 # simon.hulse@chem.ox.ac.uk
-# Last Edited: Sun 30 Jan 2022 14:37:22 GMT
+# Last Edited: Sun 30 Jan 2022 14:45:29 GMT
 
 import gzip
+from pathlib import Path
 import numpy as np
 
 
 def training_images():
-    return get_images("train-images-idx3-ubyte.gz", 60000)
+    return get_images(
+        Path(__file__).parent / "mnist_data/train-images-idx3-ubyte.gz"
+    )
 
 
 def test_images():
-    return get_images("t10k-images-idx3-ubyte.gz", 10000)
+    return get_images(
+        Path(__file__).parent / "mnist_data/t10k-images-idx3-ubyte.gz"
+    )
 
 
-def get_images(filename, n):
-    with gzip.open(f"mnist_data/{filename}", "rb") as fh:
+def get_content(path):
+    with gzip.open(path, "rb") as fh:
         content = fh.read()
     # Strip away magic number, number of images, and image dims
     # Each is a 32-bit int, so 4 bytes each -> 16 bytes
-    content = content[16:]
+    return content[16:]
 
+
+def get_images(path):
+    content = get_content(path)
     # Extract each image as a (784, 1) numpy array
     return [np.array([c for c in content[i * 784:(i + 1) * 784]]).reshape(784, 1)
-            for i in range(n)]
+            for i in range(len(content) // 784)]
